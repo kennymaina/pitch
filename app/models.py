30384885feb60768a44ccd59b663246c88e674a7
kennymaina <victormurigi@outlook.com>
2019-02-13 17:10:52 +0300

    class user added in models.py

--- app/models.py
+++ app/models.py
@@ -1,24 +1,24 @@
-# from . import db
-# from werkzeug.security import generate_password_hash,check_password_hash
-# from flask_login import UserMixin
-# from . import login_manager
-# from datetime import datetime
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(UserMixin,db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(240))
-    email = db.Column(db.String(240),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(240))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    pitches=db.relationship('Pitch',backref = 'user',lazy="dynamic")
+# class User(UserMixin,db.Model):
+#     __tablename__ = 'users'
+#     id = db.Column(db.Integer,primary_key = True)
+#     username = db.Column(db.String(240))
+#     email = db.Column(db.String(240),unique = True,index = True)
+#     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+#     pass_secure = db.Column(db.String(240))
+#     bio = db.Column(db.String(255))
+#     profile_pic_path = db.Column(db.String())
+#     pitches=db.relationship('Pitch',backref = 'user',lazy="dynamic")
    
 
     @property
