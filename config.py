9d256203f7ce044b290ef46c60ad90d5dfa4db8d
kennymaina <victormurigi@outlook.com>
2019-02-13 17:01:53 +0300

    class config added

--- config.py
+++ config.py
@@ -1,30 +1,30 @@
-# import os
+import os
 
-# class Config:
-#     '''
-#     General configuration parent class
-#     '''
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenneth:kentoz@localhost/pitch'
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-#     UPLOADED_PHOTOS_DEST ='app/static/photos'
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenneth:kentoz@localhost/pitch'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
     
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
+# class ProdConfig(Config):
+#     '''
+#     Production  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+#     Args:
+#         Config: The parent configuration class with General configuration settings
+#     '''
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenneth:kentoz@localhost/pitch_test'
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kenneth:kentoz@localhost/pitch_test'
 
 
 
